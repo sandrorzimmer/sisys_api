05caf7ad3caf7b87e7669444ca0c8430728f49f8
Sandro Zimmer <sandrorzimmer@gmail.com>
2022-11-02 09:42:42 -0300

    Changed main path from 'api/' to 'sisys/'

--- sisys_project/urls.py
+++ sisys_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('sisys_api.urls'))
+    path('sisys/', include('sisys_api.urls'))
 ]
